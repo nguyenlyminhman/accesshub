ec735df696cc44441dce60963f8a1fed2b756b6f
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-06-04 11:53:04 +0700

    init project

---
+++ src/main/java/com/spring/accesshub/AccesshubApplication.java
@@ -0,0 +1,13 @@
+package com.spring.accesshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccesshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccesshubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/accesshub/AccesshubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.accesshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccesshubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
