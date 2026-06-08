e299296ec8bd9001c8f1ccef6a789d2a20ad40b2
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-06-08 15:02:30 +0700

    common entity

--- src/main/java/com/access/hub/shared/domain/entity/BaseEntity.java
+++ src/main/java/com/access/hub/shared/domain/entity/BaseEntity.java
@@ -1,4 +1,39 @@
 package com.access.hub.shared.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    public void markAsCreated(String userId) {
+        this.createdAt = LocalDateTime.now();
+        this.createdBy = userId;
+    }
+
+    public void markAsUpdated(String userId) {
+        this.updatedAt = LocalDateTime.now();
+        this.updatedBy = userId;
+    }
 }
