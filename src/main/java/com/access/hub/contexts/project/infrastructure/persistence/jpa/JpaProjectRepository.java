bdca95bf83a2ed27042944cbbb33556aff65d19b
nguyenlyminhman <nguyenlyminhman@gmail.com>
2026-06-09 16:45:14 +0700

    create Menu use case

--- src/main/java/com/access/hub/contexts/project/infrastructure/persistence/jpa/JpaProjectRepository.java
+++ src/main/java/com/access/hub/contexts/project/infrastructure/persistence/jpa/JpaProjectRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SpringDataProjectRepository extends JpaRepository<Project, Integer> {
+public interface JpaProjectRepository extends JpaRepository<Project, Integer> {
     boolean existsByCode(String code);
 }
